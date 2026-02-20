27187999878cf781590476acf1f30440faa6db8a
YeohJK <spin8053@gmail.com>
2026-02-20 13:35:10 +0800

    Created python script azi_processing to process the logged azimuth data and calculate azimuth angle (degrees) for specific date time.

---
+++ azi_processing.py
@@ -0,0 +1,14 @@
+'''
+This file is for the processing of the logged azimuth data for
+computing the azimuth angle at a specific date and time.
+'''
+
+# Imports packages
+
+# Defines function to calculate azimuth angle (degrees) for date time
+def azimuth(dt):
+    '''
+    This function calculates azimuth angle (degrees) for specific
+    date time using interpolation of logged azimuth data.
+    '''
+    return
